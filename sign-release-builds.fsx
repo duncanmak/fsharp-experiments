dfc557d52f4103cf8491d2fd7002bebc87ef6147
Duncan Mak <duncan@a-chinaman.com>
2017-05-16 19:57:53 -0400

    Make the entrypoint work

--- sign-release-builds.fsx
+++ sign-release-builds.fsx
@@ -24,7 +24,7 @@ let signedUrl (url: Uri) (file: string) =
     Array.set segments idx (sprintf "signed/%s" (Array.last segments))
     (UriBuilder(url.Scheme, url.Host, url.Port, segments |> String.concat "")).Uri
 
-let filesInRelease (release: JsonValue) = 
+let filesInRelease (release: JsonValue) =
     [for r in release.["release"] -> r.["file"]] |> Set.ofList
 
 let replace (file: string) (dir: string) =
@@ -35,7 +35,7 @@ let replace (file: string) (dir: string) =
     let files   = filesInRelease release
     let signedFiles = DirectoryInfo(dir).EnumerateFiles() |> Seq.map (fun fi -> fi.Name) |> Set.ofSeq
     let missing = Set.difference (files |> Set.map (fun i -> i.AsString())) signedFiles
-    
+
     if Set.isEmpty missing then printfn "Skipping: %A" missing
 
     for signed in signedFiles do
@@ -47,23 +47,24 @@ let replace (file: string) (dir: string) =
 
     release.WriteTo(File.CreateText("signed-" + file), JsonSaveOptions.None)
 
-let fetch (file: string) (dir: string) = 
+let fetch (file: string) (dir: string) =
     if not (File.Exists file) then failwith (sprintf "%s is not a file" file)
     if not (Directory.Exists dir) then Directory.CreateDirectory(dir) |> ignore
-    
+
     for f in filesInRelease (JsonValue.Parse (File.ReadAllText file)) do
         let url = Uri(f.AsString())
         download url (Path.Combine(dir, url.Segments |> Array.last))
 
-#if INTERACTIVE
-#else
-[<EntryPoint; STAThread>]
-let main args = 
-    let parse file = 
-        JsonValue.Parse (File.ReadAllText file)
-
+let main (args: string[]) =
     match args with
-    | [| "fetch"; release; directory |]   -> fetch (parse release) directory
-    | [| "replace"; release; directory |] -> replace (parse release) directory
+    | [| "fetch"; release; directory |]   -> fetch release directory
+    | [| "replace"; release; directory |] -> replace release directory
     | _ -> printfn "upload-signed-builds { fetch | replace } [release json] [directory] }"
+    0
+
+#if INTERACTIVE
+main [|"hello"; "world"|]
+#else
+[<EntryPoint>]
+let entrypoint args = main args; 0
 #endif
